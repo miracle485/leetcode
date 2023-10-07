3c21d314412c13963fa7425ee46c901cf1815ff0
wangyilin <wangyilin8@xiaomi.com>
2023-10-07 17:44:24 +0800

    feat：每日一题&hot100

--- src/main/java/leetcode/LeetCode901.java
+++ src/main/java/leetcode/LeetCode901.java
@@ -3,14 +3,30 @@ package leetcode;
 public class LeetCode901 {
     private int preInt = 0;
     private int preDay = 1;
+    private int[] prices = new int[10000];
+    private int[] kc = new int[10000];
+    private int flag = 0;
 
     public int next(int price) {
-        if (price < preInt) {
-            preDay++;
-        } else {
-            preDay = 1;
+        return 0;
+    }
+
+    public int next2(int price) {
+        int sum = 0;
+        kc[flag] = 1;
+        prices[flag] = price;
+        int now = flag;
+        while (now >= 0 &&prices[now] <= price) {
+            sum++;
+            now -= kc[now];
         }
-        preInt = price;
-        return preDay;
+        flag++;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        LeetCode901 leetCode901 = new LeetCode901();
+        System.out.println(leetCode901.next2(100));
+        System.out.println(leetCode901.next2(80));
     }
 }
